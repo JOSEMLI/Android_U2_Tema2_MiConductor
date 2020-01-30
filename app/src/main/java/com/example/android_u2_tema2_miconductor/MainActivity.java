d94b92076dd86a34837dca1b1aa3884a0269d1ef
Jose Mamani <JOSEMLI@example.com>
2020-01-30 11:18:08 -0500

    se modifico el buil el layout y el activity

--- app/src/main/java/com/example/android_u2_tema2_miconductor/MainActivity.java
+++ app/src/main/java/com/example/android_u2_tema2_miconductor/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.android_u2_tema2_miconductor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+public class MainActivity  extends AppCompatActivity implements OnMapReadyCallback {
+  LatLng pos;
+  GoogleMap mapa;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    SupportMapFragment mapFragment = (SupportMapFragment)
+        getSupportFragmentManager().findFragmentById(R.id.mapa);
+    mapFragment.getMapAsync(this);
+  }
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    mapa = googleMap;
+    pos = new LatLng(-18.011737, -70.253529);
+    mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(pos,17));
+    if (ContextCompat.checkSelfPermission(this,
+        android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+        PackageManager.PERMISSION_GRANTED) {
+      mapa.setMyLocationEnabled(true);
+      mapa.getUiSettings().setZoomControlsEnabled(false);
+      mapa.getUiSettings().setCompassEnabled(true);
+    }
+  }
+  public void pedir(View view) {
   }
 }
